fdcb06070d79a6f182102e773ea380e39d79bc04
jordanoboyle <jmcmcode@gmail.com>
2024-04-23 20:34:37 -0600

    Geocode-Run initial commit

---
+++ Geocoder-Fun/geocode-test.rb
@@ -0,0 +1,7 @@
+require "geocoder"
+
+pp Geocoder.search("6575 Dunbarton Drive, Hudson, Ohio")
+
+pp Geocoder.search("768 Stacy Oak Way, Millersville, Maryland")
+
+pp Geocoder.search("10 Hacienda Caballero, Santa Fe, NM")
