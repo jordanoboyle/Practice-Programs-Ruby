57b5aa774600b20eb7909b85fb59e99806812f5e
jordanoboyle <jmcmcode@gmail.com>
2024-05-07 20:11:54 -0600

    Example of how floats must be defined. Downcase example added

--- deliberate-practice-ruby/Basic/100variable-define.rb
+++ deliberate-practice-ruby/Basic/100variable-define.rb
@@ -55,7 +55,36 @@ p string1.length
 
 # # 8 Write a program that uses a variable to store a number with decimals, then prints the number as an integer.
 
+phi = 1.618
+proper_even = 2
+
+proper_odd = 3
+
+p phi * proper_even
+p phi / proper_even
+p phi + proper_even
+p phi - proper_even
+
+p (proper_even * proper_odd)
+p (proper_even.to_f / proper_odd)  # this is important in recognizing that you need to have floats to get proper decimal answers
+p proper_even + proper_odd
+p proper_even - proper_odd
+
 # # 9 Write a program that uses a variable to store two numbers, then prints the two numbers multiplied together.
+
+number_float = 3.5
+number_int = 5
+num_turn_to_float = 6
+
+puts "This is what happens when you mult integers #{number_int * num_turn_to_float}"
+puts "This is what happens when you multi a float and an integer #{number_float * number_int}"
+
+pp "No let's convert the divide an integer by a greater integer #{number_int / num_turn_to_float}. This does not actually equal zero"
+pp "Let's make the change here #{number_int.to_f / num_turn_to_float}. See result"
+
 
 # # 10 Write a program that uses a variable to store a word, then prints the word with all lowercase letters.
 
+legend = "This is the Legend of Zelda BOTW"
+
+p legend.downcase
