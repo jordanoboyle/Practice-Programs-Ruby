574e337c4e1ada58a146111f8482e87c1d4a1255
jordanoboyle <jmcmcode@gmail.com>
2024-05-07 18:25:41 -0600

    test teaching loops. while, for, times, and boolean

--- test.rb
+++ test.rb
@@ -1,3 +1,29 @@
-def this_is_a_test
-  
+
+
+name = ["Leslie", "Harley", "Jordan", "Gotham"]
+
+name1 = "Harley"
+
+#index = 0
+#while index < 10 
+#  puts name1
+#  index = index + 1 
+#end
+
+# 10.times do 
+#   puts name1
+# end
+
+
+# for appple in Array(1..10)
+#   puts name1
+# end
+
+index = 0 
+while true
+  puts "floof"
+  index = index + 1
+  if index == 10
+    break
+  end
 end
